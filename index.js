169c2e474a497ad9d5fbd23d45c803347f690ae6
Ochoooooo <greenfieldrp7@gmail.com>
2024-12-09 11:08:36 +0100

    Upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import chalk from 'chalk';
+
+const jonathan = chalk.red("jonathan");
+const julien = chalk.green("julien");
+const theophile = chalk.yellow("théophile");
+const lisa = chalk.blue("lisa");
+
+console.log(jonathan + julien + theophile + lisa);
+
+
+
